f7d5bf68c04bc0778e74a967fb650fbda483ab7b
jagadish290 <jagadish@appinessworld.com>
2019-08-26 09:09:41 +0530

    second commit

---
+++ birthday.js
@@ -0,0 +1,14 @@
+function birthdayCakeCandles(arr) {
+    let maximum = 0;
+    let i = 0;
+    arr.forEach(item => {
+        if (item > maximum) {
+            max = item;
+            i = 1;
+        } else if (item === maximum) {
+            i++;
+        }
+    });
+
+    return i;
+}
